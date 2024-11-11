3f4fe97b7dbc3994ce11fd41a5248f38ae0dbcd9
Sennaf0x <Sennafo@hotmail.com>
2024-11-11 08:46:26 -0400

    Adicionando style.css

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
+with open("style.css") as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 st.markdown('''
             <style>
                 .e1f1d6gn3{
